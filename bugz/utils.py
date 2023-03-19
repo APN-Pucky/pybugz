84ce42c07923a54f7de274c21fc3896c596abf3e
Alexander Puck Neuwirth <APN-Pucky@users.noreply.github.com>
2023-03-19 15:22:08 +0100

    Update utils.py

--- bugz/utils.py
+++ bugz/utils.py
@@ -47,8 +47,8 @@ def raw_input_block():
 
 def terminal_width():
     """Return estimated terminal width."""
-    if sys.platform == 'win32':
-        return win32utils.get_console_size()[0]
+    #if sys.platform == 'win32':
+    #    return win32utils.get_console_size()[0]
     width = DEFAULT_NUM_COLS
     try:
         import struct
